678c776338f14b8eb3edff14d54265bb286c8a92
madhumitharajendran-94 <madhumitharajendran973@gmail.com>
2025-05-20 14:44:47 +0530

    DAY07

---
+++ placements/file4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<3;i++){
+        for(int j=i;j<6;j++){
+            if(i%2==0)printf("hi");
+            if(j%2==0)printf("hello");        }
+        
+    }
+    
+}
